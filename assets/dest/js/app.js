6ac72148e387c8547f69cec403c193f41d854947
Gaurav Aggarwal <msgauravcool@gmail.com>
2022-06-23 19:15:30 +0530

    Fixed folder structure

--- assets/dest/js/app.js
+++
@@ -1,1 +0,0 @@
-$("#view-all-services").click(function(){$(".popup-services-container").css("display","block")}),$(".remove-popup").click(function(){$(".popup-services-container").css("display","none")}),$("#contact-us-form").submit(function(){alert("Contact form has been submitted successfully.")});
--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,35 @@ const concatJs = require("gulp-concat");
 const browserSync = require("browser-sync").create();
 
 // Complie SASS files to Stylesheets with Autoprefixer
-gulp.task("styles", function() {
-	return gulp.src("assets/src/sass/*.scss")
-		.pipe(sass().on("error", sass.logError))		// gulp-sass plugin
-		.pipe(autoprefixer("last 2 versions"))			// gulp-autoprefixer plugin
-		.pipe(gulp.dest("assets/dest/css"))
-		.pipe(browserSync.stream());								// browser-sync plugin
+gulp.task("styles", function () {
+  return gulp.src("assets/src/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))		// gulp-sass plugin
+    .pipe(autoprefixer("last 2 versions"))			// gulp-autoprefixer plugin
+    .pipe(gulp.dest("assets/dest/css"))
+    .pipe(browserSync.stream());								// browser-sync plugin
 });
 
 // Concat JS with Minify JS
-gulp.task("concat-minify-js", function() {
-	return gulp.src("assets/src/js/*.js")
-		.pipe(concatJs("app.js"))										// gulp-concat plugin
-		.pipe(uglify()) 														// gulp-uglify plugin
-		.pipe(gulp.dest("assets/dest/js"))
-		.pipe(browserSync.stream());								// browser-sync plugin
+gulp.task("concat-minify-js", function () {
+  return gulp.src("assets/src/js/*.js")
+    .pipe(concatJs("app.js"))										// gulp-concat plugin
+    .pipe(uglify()) 														// gulp-uglify plugin
+    .pipe(gulp.dest("assets/dest/js"))
+    .pipe(browserSync.stream());								// browser-sync plugin
 });
 
 // Browser Sync Task with Gulp Watcher
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 
-    // gulp-watch API
-    gulp.watch("./*.html").on("change", browserSync.reload);
-    gulp.watch("assets/src/sass/*.scss", gulp.series("styles"));
-    gulp.watch("assets/src/js/*.js", gulp.series("concat-minify-js"));
+  // gulp-watch API
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("assets/src/sass/*.scss", gulp.series("styles"));
+  gulp.watch("assets/src/js/*.js", gulp.series("concat-minify-js"));
 });
 
 // Default Gulp Task
